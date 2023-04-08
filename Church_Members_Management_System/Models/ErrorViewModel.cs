01ad142524b560db43d796706c1ad5b50cdfc28f
Thaboh <Thaboh@DESKTOP-4SCHNTH>
2023-04-08 11:03:23 +0200

    Add project files.

---
+++ Church_Members_Management_System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Church_Members_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
